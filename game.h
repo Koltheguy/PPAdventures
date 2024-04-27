b89d1f54c433c6d00ea8542f7a81fc68bf5cf71e
hero346 <efcasillas1@hotmail.com>
2024-04-27 16:58:35 -0700

    Revert "Implementing rendering of player scores"
    This reverts commit 4ab709e4dfb7393f69df5e87ec842f84584cd2ac.

--- game.h
+++ game.h
@@ -5,8 +5,6 @@
 #include "player.h"
 #include "ball.h"
 
-static const float SCORE_DISTANCE = 0.6f;
-
 class Game
 {
 
@@ -17,20 +15,6 @@ public:
 
 	static void getKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
 	static bool getKey(int keyI);
-	GLuint gameVAO, gameVBO;
-	GLfloat gameVertices[7 * 3]; // seven vertices to draw the score each with x, y and z coords
-	void render(PlayerSide sideWon, int score);
-	void draw(PlayerSide sideWon, int score);
-	void zeroVertices(float xspacing, float yspacing);
-	void oneVertices(float xspacing, float yspacing);
-	void twoVertices(float xspacing, float yspacing);
-	void threeVertices(float xspacing, float yspacing);
-	void fourVertices(float xspacing, float yspacing);
-	void fiveVertices(float xspacing, float yspacing);
-	void sixVertices(float xspacing, float yspacing);
-	void sevenVertices(float xspacing, float yspacing);
-	void eightVertices(float xspacing, float yspacing);
-	void nineVertices(float xspacing, float yspacing);
 
 protected:
 	Ball ball;
